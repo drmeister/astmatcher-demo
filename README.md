77e34a427afb6ff10f8341d24f4e7c1e084e49ef
Georgiy Tugai <georgiy.tugai@gmail.com>
2015-04-26 17:05:27 +0930

    Improved documentation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-A demo written by Georgey Tagai that uses the Clang ASTMatcher facility within Clasp to parse and analyze C++ code.
+A demo written by [Georgiy Tugai](https://github.com/Georgiy-Tugai) that uses the [Clang ASTMatcher](http://clang.llvm.org/docs/LibASTMatchersReference.html) facility within [Clasp](https://github.com/drmeister/clasp) to parse and analyze C++ code.
+
+Before running this on your own machine, make sure to update the `directory` key in `compile_commands.json` to the absolute path containing this file.
--- test.lisp
+++ test.lisp
@@ -20,21 +20,21 @@
 (progn
   (format t "---- 2. Defining the matcher for class/field/method~%")
   (defparameter *matcher*
-    '(:record-decl 
-      (:for-each 
+    '(:record-decl                      ; Class declarations...
+      (:for-each                        ; match their contents, once per item
        (:any-of 
-	(:bind :field 
+	(:bind :field                       ; Store field (member) declarations in :field
 	       (:field-decl
-		(:unless 
-		    (:any-of 
-		     (:has-ancestor 
+		(:unless                        ; We don't care about
+		    (:any-of
+		     (:has-ancestor             ; template specializations
 		      (:class-template-specialization-decl))
-		     (:is-implicit)))))
-	(:bind :method
-	       (:method-decl
+		     (:is-implicit)))))         ; or implicit fields
+	(:bind :method                      ; Store method declarations in :method
+	       (:method-decl                ; and their return types in :ret-type
 		(:unless (:is-implicit))
 		(:returns (:bind :ret-type (:qual-type)))))))))
-  ;; Matcher for function/method parameters
+  ;; Matcher for method parameters; may be easily extended to functions
   (defparameter *method-parm-matcher*
     (compile-matcher 
      '(:method-decl 
@@ -49,10 +49,12 @@
 (progn
   (format t "---- 3. Define some helper functions~%")
   (defun mtag-node-or-null (tag)
+    "Return a matched node, or NIL if that node does not exist."
     (handler-case (mtag-node tag)
       (no-node-for-tag-error (err) nil)))
 
   (defun ast-node-typed-decl (node)
+    "Return (cons type name) for a value-decl"
     (cons (clang-ast:get-as-string
 	   (clang-ast:get-type node))
 	  (clang-ast:get-name node)))
@@ -68,15 +70,16 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; Define some helper functions
+;;; Define the search function
 ;;;
 (progn
-  (format t "---- 4. Define some helper functions~%")
+  (format t "---- 4. Define the search function~%")
   (defun get-classes ()
     (let ((classes (make-hash-table :test 'equal)))
       (match-run
        *matcher*
        :code (lambda ()
+               ;; This code is run for each match.
 	       (let* ((field-node (mtag-node-or-null :field))
 		      (method-node (mtag-node-or-null :method))
 		      (ast-name (mtag-name :whole))
